b9a68c44dc929bc6c4416c4ddb6717bc07fcc1b7
jiangkn <jiangkn>
2024-12-10 10:48:18 +0800

    add nushell

--- config/launch.lua
+++ config/launch.lua
@@ -6,12 +6,12 @@ local options = {
 }
 
 if platform.is_win then
-   options.default_prog = { 'pwsh', '-NoLogo' }
+   options.default_prog = { 'nu' }
    options.launch_menu = {
       { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
       { label = 'Command Prompt', args = { 'cmd' } },
-      -- { label = 'Nushell', args = { 'nu' } },
+      { label = 'Nushell', args = { 'nu' } },
       -- {
       --    label = 'Git Bash',
       --    args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
